61431921566f8b9652fa120938616834bf07639c
Chris Stamper <chris@cdstamper.co>
2019-09-18 14:14:13 -0400

    fix: #296 allow delegate to handle autocomplete shouldChange

--- PhoneNumberKit/UI/TextField.swift
+++ PhoneNumberKit/UI/TextField.swift
@@ -212,7 +212,12 @@ open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
 
     open func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
-        // This allows for the case when a user autocompletes a phone number:
+        // allow delegate to intervene
+        guard _delegate?.textField?(textField, shouldChangeCharactersIn: range, replacementString: string) ?? true else {
+            return false
+        }
+        
+        // iOS autocomplete seems to insert a trailing space:
         if range == NSRange(location: 0, length: 0) && string == " " {
             return true
         }
@@ -221,10 +226,6 @@ open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
             return false
         }
 
-        // allow delegate to intervene
-        guard _delegate?.textField?(textField, shouldChangeCharactersIn: range, replacementString: string) ?? true else {
-            return false
-        }
         guard isPartialFormatterEnabled else {
             return true
         }
